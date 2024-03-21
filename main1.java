956b2c162d1be97aaf4860d99b7c2af1357d5294
AlyzaC <313981872@student.baisd.net>
2024-03-21 14:32:27 -0400

    Added to outline

--- main1.java
+++ main1.java
@@ -7,36 +7,117 @@ public static void main(String[] args){
     //Variables & Objects
 
     //Scanner(s)?
+        //Scanner kbd = new Scanner(System.in)
 
     //Login
 
-    /* First menu
+    
+    /* Main menu
      * - Astronauts
      * - Rocket/Ship Inventory
      * - Launch
-     */
+     * - Quit
+    do {
+        System.out.println(String for main menu);
+        int choice = kbd.nextInt();
+        switch (choice) {
+            case 1:
+                do {
+                    System.out.println(astro menu);
+                    int choice2 = kbd.nextInt();
+                    switch (choice2) {
+                    case 1:
+                        Code for adding astro
+                        break;
 
-    /* Astronauts menu
-     * - Add
-     * - Edit info
-     * - Delete
-    */
-    //Code for each option
+                    case 2:
+                        code for editing astro
+                        break;
 
-    /* Rocket/Ship menu
-     * - Add
-     * - Edit info
-     * - Delete 
-     */
-    //Code for each option
+                    case 3:
+                        code for deleting astro
+                        break;
 
-    /* Launch
-     * - Fuel? (Does it make more sense for fuel to be here or in the ship inventory?)
-     *    - Check fuel
-     *    - Add fuel
-     * - Launch
-     */
-    //Code for each option
+                    case 4:
+                        System.out.println(back);
+                        break;
+
+                    default:
+                        while (choice2 > 4 || choice2 < 1) {
+                            System.out.println("Please enter a number from 1-4");
+                        }
+                        break;
+                    }
+                } while ( != 4);
+                break;
+
+            case 2:
+                * Rocket/Ship menu
+                * - Add
+                * - Edit info
+                * - Delete 
+                do{
+                    System.out.println(string for rocket/ship menu);
+                    int choice2 = kbd.nextInt();
+                    switch (choice2) {
+                        case 1:
+                            break;
+
+                        case 2:
+                            break;
+
+                        case 3:
+                            break;
+
+                        case 4:
+                            break;
+
+                        default:
+                            break;
+                    }
+                } while ( != 4)
+                break;
+
+            case 3:
+                * Launch
+                * - Fuel? (Does it make more sense for fuel to be here or in the ship inventory?)
+                *    - Check fuel
+                *    - Add fuel
+                * - Launch
+                * Rocket/Ship menu
+                * - Add
+                * - Edit info
+                * - Delete 
+                do {
+                    System.out.println(string for launch menu);
+                    int choice2 = kbd.nextInt();
+                    switch (choice2) {
+                        case 1:
+                            break;
+
+                        case 2:
+                            break;
+
+                        case 3:
+                            break;
+
+                        case 4:
+                            break;
+
+                        default:
+                            break;
+                    }
+                } while ( != 4)
+                break;
+
+            case 4:
+                System.out.println();
+                break;
+
+            default:
+                break;
+        }
+    }*/
 
     //Close scanners and program
 }
