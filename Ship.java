dd39c246327950244a60286da4f185acb059fae6
Somebodythatisalive <163891665+Somebodythatisalive@users.noreply.github.com>
2024-04-25 12:19:00 -0400

    AddAstro

--- Ship.java
+++ Ship.java
@@ -84,10 +84,10 @@ public void Space()
 
  
     System.out.println("Please choose an astronaut to go on a spacewalk.");
-    //Scanner will show a menu of all of the Strings in crew[]
+     //Scanner will show a menu of all of the Strings in crew[]
     //scanner will check to make sure astronauts name/id is valid
-    //if valid a 5 minute timer starts and boolean spacewalk is true
-    //if notvalid Code will state that no astronaut name could be found and will ask them to try again
+   //if valid a 5 minute timer starts and boolean spacewalk is true
+  //if notvalid Code will state that no astronaut name could be found and will ask them to try again
     System.out.println("(person stuff here) Has been sent on a spacewalk");
     spacewalk = true;
     //after 5 minutes the astronaut will return to the ship
@@ -102,11 +102,11 @@ public void Space()
     returning = true;
   }
   // if spaceWalkComplete is false and returning is true whichever astronaut went on the spacewalk has to be removed as AstroRemoval.java
-  if (spaceWalkComplete = false && returning ==true)
+  if (spaceWalkComplete == false && returning ==true)
   {
    System.out.println ("The Spacewalking astronaut failed to get in the ship before reentry started.");
    System.out.println("We can only hope his death was quick and painless");
-   //Tell astroRemoval to remove the astronaut
+   //Tell AstroRemoval.java to remove the astronaut
   }
 }
 
@@ -128,7 +128,11 @@ public void ship(int speed,boolean inSpace )
  {
   System.out.println ("The ship has landed sucessfully");
   doorIsOpen = true;
-  //clear the array "crew" and set fuel to zero
+  //clear the String array "crew" and set fuel to zero
+  for (int i = 0; i < crew.length; i++)
+  {
+    crew[i] = null;
+  }
 
   fuel = 0;
  }
@@ -142,17 +146,19 @@ public void ship(int speed,boolean inSpace )
 
 
 
-public void addAstro()
+public void addAstro(Astronaut astronaut)
 {
   // if main1.java sends a perimeter to add an astronaut
 if (placeHolder == 3)
-{
-  if (shipCapacity > crewNum)
-  {
+{ if (shipCapacity > crewNum)
+  { if (crew == null)
+    {
+      crew = new Astronaut[shipCapacity];
+    }
+    crew[crewNum] = astronaut;
+    crewNum++;
  //The code will add this astro sent by main to the array "crew"
  System.out.println ("astronaut has been added to ship.");
-  
-
 } else
 {
   // the astro is not added to the array
@@ -160,11 +166,8 @@ if (placeHolder == 3)
 }
  }
 } 
-
-
 public void launch()
 {
-  
 if (placeHolder == 4) // when a perimeter is sent from main1 to launch...
 {
   //The code will first make sure that crewNum is equal to shipCapacity
@@ -179,21 +182,16 @@ System.out.println("The Ship needs more fuel for a sucessful launch.");
  }
  else {
 // if fuel = fuelCapacity and crewNum = shipCapacity the launch will start
-
-  
   System.out.println ("launching metal tube into orbit");
-
 /* accel and decell code here.
 altitude = altitude + speed;
 speed = speed + accel - deccel;
 tldr.  +speed is up -speed is down 
 thats why the parachute gives -7 deccel
  */
-
  }
 }
 }
-
 public void deployParachutes()
 { // if altitude goes lower than 10000 while returning is true parachutes will be deployed
   if (altitude < 10000 && returning == true)
@@ -211,7 +209,7 @@ public void deployParachutes()
    // if failure is ever set to true
     System.out.println  ("All astronauts did not survive");
     System.out.println ("womp womp");
-    // clear crew, crewNum, fuelcapacity, shipname , shipcapacity, fuel
+    // clear String crew, Int crewNum, double fuelcapacity, String shipname , int shipcapacity, dobule fuel
     //tell astroremoval to remove all astros that were part of crew
     //tell shipremoval to remove the ship
     //set failure back to false once everything is done so that way the next test dosent automatically fail
